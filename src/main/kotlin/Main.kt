e4367a64df8ea1aa7d80791457097d9b73f73aa1
student <mjeraeunice@gmail.com>
2023-02-03 14:22:57 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Bela Moore"
+    println(fullName)
+    var age: Byte = 22
+    println(age)
+    var phoneNumber: Long = +254723467512
+    println(phoneNumber)
+    var weightinKg: Byte = 45
+    println(weightinKg)
+    var kenyanCitizen: Boolean = true
+    println(kenyanCitizen)
+}
